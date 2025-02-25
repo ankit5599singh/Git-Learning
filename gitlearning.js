c50cc287f10237996cb89700f00869222f51eda9
Ankit Singh <ankit5599singh@gmail.com>
2025-02-25 15:07:25 +0530

    This is Initial Commit

---
+++ gitlearning.js
@@ -0,0 +1,21 @@
+// GIT Learning
+// git init
+// In order to initialize new GIT Git Repository we have to use "git init" command in the bash ternimal and .git file will be created in the same directory which keep tracks of version of our project.
+
+// git branch -m main
+// By default the git creates the main branch as master using "git branch -m main" command we can change the name of the main branch as per your choice.
+
+// git status
+// By using "git status" command we can check the git branches status including main branch name
+
+// git status -s
+// By using "git status -s" command we can check the git branches status
+
+// git add gitlearning.html
+// By default all the files in git branch is untracted so we have to use "git add" to file in staging area
+// git add gitlearning.html git.js.
+// This command add multiple files in staging area
+// git add.
+// This command add all files to staging area
+
+//
